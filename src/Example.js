d901baebba5f6ce553eace0177a54a4731b59804
Raul Cudris <raulcudriz@hotmail.com>
2022-11-03 16:02:15 -0500

    Creacion de archivo Example.js

---
+++ src/Example.js
@@ -0,0 +1,15 @@
+
+export async function runCode(url) {
+    try {
+     new URL(url);
+     const response = await fetch(url);
+     return response.json();
+    } catch(error) {
+      if (error.message === "Failed to construct 'URL': Invalid URL"){
+        throw new Error('Invalid URL');
+      } else {
+        throw new Error('Something was wrong');
+      }
+    }
+  }
+  
